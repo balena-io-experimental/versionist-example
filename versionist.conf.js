3c0232140b5df6f41031785baadb6469d914f6af
Juan Cruz Viotti <jviotti@openmailbox.org>
2016-11-11 00:26:17 +0200

    v2.0.0
    Signed-off-by: Juan Cruz Viotti <jviotti@openmailbox.org>

--- versionist.conf.js
+++ versionist.conf.js
@@ -5,12 +5,40 @@ module.exports = {
 
   addEntryToChangelog: 'prepend',
 
+  transformTemplateData: (data) => {
+    data.features = data.commits.filter((commit) => {
+      return commit.subject.startsWith('feat:');
+    }).map((commit) => {
+      return commit.subject.slice(6);
+    });
+
+    data.fixes = data.commits.filter((commit) => {
+      return commit.subject.startsWith('fix:');
+    }).map((commit) => {
+      return commit.subject.slice(5);
+    });
+
+    return data;
+  },
+
   template: [
     '## v{{version}} - {{moment date "Y-MM-DD"}}',
+    '{{#if features.length}}',
     '',
-    '{{#each commits}}',
-    '- {{capitalize this.subject}}',
-    '{{/each}}'
+    '### Features',
+    '',
+    '{{#each features}}',
+    '- {{capitalize this}}',
+    '{{/each}}',
+    '{{/if}}',
+    '{{#if fixes.length}}',
+    '',
+    '### Fixes',
+    '',
+    '{{#each fixes}}',
+    '- {{capitalize this}}',
+    '{{/each}}',
+    '{{/if}}'
   ].join('\n')
 
 };
